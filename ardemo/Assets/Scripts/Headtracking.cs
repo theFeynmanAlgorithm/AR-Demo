ea46ee39804f6840b36c03194cee12507226ab4a
ZWRgamer <zwr936@gmail.com>
2016-11-26 12:31:05 -0500

    Project Upload
    Upload to GitHub after initial work

---
+++ ardemo/Assets/Scripts/Headtracking.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Headtracking : MonoBehaviour {
+
+    //public Text rotX;
+    //public Text rotY;
+    //public Text rotZ;
+
+    [Range(0f,1f)]
+    public float smoothingFactor;
+    
+    float smoothedRotationX;
+    float smoothedRotationY;
+    float smoothedRotationZ;
+
+    void Start()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        Input.gyro.enabled = true;
+
+        smoothedRotationX = Input.acceleration.x * 90f;
+        smoothedRotationY = Input.acceleration.y * 90f;
+        smoothedRotationZ = Input.gyro.attitude.eulerAngles.z - 180f;
+    }
+
+	void Update()
+    {
+        float xRotate = Input.acceleration.x * 90;
+        float yRotate = Input.acceleration.y * 90;
+        float zRotate = Input.gyro.attitude.eulerAngles.z - 180f;
+        
+        smoothedRotationX = smoothingFactor * xRotate + (1 - smoothingFactor) * smoothedRotationX;
+        smoothedRotationY = smoothingFactor * yRotate + (1 - smoothingFactor) * smoothedRotationY;
+        smoothedRotationZ = smoothingFactor * zRotate + (1 - smoothingFactor) * smoothedRotationZ;
+
+        transform.rotation = Quaternion.Euler(new Vector3(smoothedRotationY, smoothedRotationZ, smoothedRotationX));
+        //rotX.text = "X: " + smoothedRotationX.ToString("F1");
+        //rotY.text = "Y: " + smoothedRotationY.ToString("F1");
+        //rotZ.text = zRotate.ToString("F1");
+    }
+}
---
+++ ardemo/Assets/Scripts/move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, .5f, 0f));
+        //Vector3 newPos = new Vector3(0f, 0f, Mathf.PingPong(Time.time, 5) + 2f);
+        //transform.position = newPos;
+    }
+}
